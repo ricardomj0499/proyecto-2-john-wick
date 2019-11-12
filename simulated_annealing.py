dbe94a557bd59717dafd997c6bdb3538545bea42
Roy8743 <57109928+Roy8743@users.noreply.github.com>
2019-11-11 19:41:14 -0600

    Update simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,4 +1,4 @@
-def optimizar(dominio, temperatura = 10e32, tasa_enfriamiento = 0.95):
+#def optimizar(dominio, temperatura = 10e32, tasa_enfriamiento = 0.95):
     """Algoritmo de optimización estocástica simulated annealing.
 
     Entradas:
@@ -16,6 +16,20 @@ def optimizar(dominio, temperatura = 10e32, tasa_enfriamiento = 0.95):
         (estructura de datos) Estructura de datos según el dominio, que representa una
         aproximación a la mejor solución al problema.
     """
-
-    # Pendiente: implementar esta función
-    pass
+    def optimizar(dominio, temperatura=10e26,tasa_enfriamiento=0.999999):
+        sol=dominio.generar()
+	    costo=dominio.fcosto(sol)
+        while(temperatura>=0.001):
+            sol_prima=dominio.vecino(sol)
+            costo_prima=dominio.fcosto(sol_prima)
+            p=math.exp(-abs(costo_prima-costo)/temperatura)
+            p_azar= random.randint(0,1)
+            #print("uno: "+str(costo)+".....dos: "+str(costo_prima))
+            if(costo_prima<=costo or p_azar<p):
+                sol=sol_prima
+                costo=costo_prima	
+            temperatura=temperatura*tasa_enfriamiento
+            #print(temperatura)
+        return "Solucion: "+str(sol)+"\nCosto: "+str(costo)
+    
+    
