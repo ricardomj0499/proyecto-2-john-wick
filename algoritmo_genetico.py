7fa43ac41e4b20808c3f9a26052fd9ff0062a163
Roy8743 <57109928+Roy8743@users.noreply.github.com>
2019-11-22 23:32:22 -0600

    Update algoritmo_genetico.py

--- algoritmo_genetico.py
+++ algoritmo_genetico.py
@@ -1,4 +1,5 @@
-
+import math
+import random
 def optimizar(dominio, tam_pobl, porc_elite, prob_mut, reps):
     """Algoritmo genético para optimización estocástica.
 
@@ -22,6 +23,42 @@ def optimizar(dominio, tam_pobl, porc_elite, prob_mut, reps):
         (estructura de datos) Estructura de datos según el dominio, que representa una
         aproximación a la mejor solución al problema.
     """
-
+    pobl=dominio.generar_n(tam_pobl)
+    aptitud_de_poblacion=[]
+    while(reps>0):
+        for i in(pobl):
+            #print(pobl)
+            aptitud_de_poblacion.append(dominio.fcosto(i))
+        #print(len(aptitud_de_poblacion))
+        #print(len(pobl))
+        ordenar=dominio.mergeSort_doble(aptitud_de_poblacion,pobl)
+        #print(ordenar)
+        #print(ordenar)
+        pobl=ordenar[0]
+        aptitud_de_poblacion=ordenar[1]
+        tamano_poblacion=len(pobl)
+        num_padres=math.floor((tamano_poblacion*porc_elite))
+        num_hijos=(tamano_poblacion-num_padres)
+        sig_gen=pobl[0:num_padres]
+        decendencia=[]
+        while(num_hijos>0):
+            #print(sig_gen)
+            padre_a=random.choice(sig_gen)
+            padre_b=random.choice(sig_gen)
+            hijo=dominio.cruzar(padre_a,padre_b)
+            p=random.uniform(0,1)
+            if(p<=prob_mut):
+                hijo=dominio.mutar(hijo)
+            decendencia+=[hijo]
+            #print(hijo)
+            num_hijos-=1
+        pobl=sig_gen+decendencia
+        aptitud_de_poblacion=[]
+        print(reps)
+        reps-=1
+		
+	
+	#dominio.ordenar(pobl,llave=aptitud)
+			
     # Pendiente: implementar este método
-    pass
+    #pass
